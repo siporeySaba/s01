d3b962950fb42e92f38f91d92eba3166e0a02eb5
siporeySaba <siporeysaba@gmail.com>
2026-06-05 04:30:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installed");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated");
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  // כרגע לא חוסמים או שומרים קאש
+  // מאפשר שליטה מלאה בדף
 });
