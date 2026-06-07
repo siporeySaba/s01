5f75a1a73e6ee642232b5dfd60782b8165b6f9f0
siporeySaba <siporeysaba@gmail.com>
2026-06-07 21:47:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,40 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+const CACHE_NAME = "saba-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./episode.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  // מאפשר שליטה מלאה בדף
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 });
