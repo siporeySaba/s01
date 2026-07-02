160ebe4985ffa9ac39419bebf622f6f182493153
siporeySaba <siporeysaba@gmail.com>
2026-07-02 17:21:18 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
